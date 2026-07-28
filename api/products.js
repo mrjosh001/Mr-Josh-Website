035474ebaadf27572944ed5a3d80d498dd442a8d
Mr Josh <agaziejoseph0@gmail.com>
2026-07-28 15:27:01 +0100

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,11 +1,11 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json');
 
   const apiKey = 'rsk_live_s5ATWd0yskBvEagViPwQd6HxwfdLrkkpGyZIZFXDnhPEj8W6';
-  
+
   try {
-    const apiRes = await fetch('https://fadded.net/api/v1/reseller/products', {
+    const response = await fetch('https://fadded.net/api/v1/reseller/products', {
       method: 'GET',
       headers: {
         'X-Api-Key': apiKey,
@@ -13,9 +13,16 @@ module.exports = async function handler(req, res) {
       }
     });
 
-    const data = await apiRes.json();
+    const text = await response.text();
+    let data;
+    try {
+      data = JSON.parse(text);
+    } catch (e) {
+      data = { raw: text };
+    }
+
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
-};
+}
