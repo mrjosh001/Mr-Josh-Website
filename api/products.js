728cd87ab2f7b88c40067d49e74a0a406f788df3
Mr Josh <agaziejoseph0@gmail.com>
2026-07-28 15:21:20 +0100

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json');
 
@@ -18,4 +18,4 @@ module.exports = async function handler(req, res) {
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
-};
+}
